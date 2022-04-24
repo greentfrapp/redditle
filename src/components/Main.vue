a5bfc3dc344f305b5d90ca1fb19267af9b3dd04d
SK <limsweekiat@gmail.com>
2022-04-25 00:06:36 +0800

    Add dark mode

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,19 +1,31 @@
 <template>
-  <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-4 items-center">
-    <h1 class="font-medium text-5xl sm:text-6xl font-title">
-      <span class="text-red-500">reddit</span>le
-    </h1>
-    <div class="relative block max-w-screen px-4 sm:px-0 w-[35rem] text-gray-800">
-      <div class="absolute inset-y-0 left-0 pl-8 sm:pl-4 flex items-center">
-        <SearchIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
-      </div>
-      <input autofocus v-model="query" type="text" name="query" @keyup.enter="submit"
-        placeholder="Try prefixing with r/<subreddit> for a specific subreddit"
-        class="block w-full border border-gray-300 rounded-full py-3 pl-12 hover:shadow-catche focus:shadow-around focus:ring-0 focus:border-white focus-visible:outline-none" />
+  <div class="h-screen w-screen" :class="[darkMode ? 'bg-neutral-800' : 'bg-white']">
+    <div class="flex gap-1 items-center w-full justify-end p-2">
+      <SunIcon class="h-5" :class="[darkMode ? 'text-neutral-600' : 'text-neutral-300']" />
+      <Switch v-model="darkMode" :class="[darkMode ? 'bg-neutral-600' : 'bg-neutral-200', 'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-0']">
+        <span class="sr-only">Use setting</span>
+        <span aria-hidden="true" :class="[darkMode ? 'translate-x-5' : 'translate-x-0', 'pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200']" />
+      </Switch>
+      <MoonIcon class="h-5" :class="[darkMode ? 'text-neutral-600' : 'text-neutral-300']" />
     </div>
-    <div class="flex flex-col gap-2">
-      <button class="text-gray-800 bg-gray-100 px-4 py-2 rounded hover:shadow-around border border-gray-300" @click="submit">Search</button>
-      <a class="w-max text-red-500 text-sm" href="https://www.producthunt.com/posts/redditle" target="_blank">(Psst we're on ProductHunt!)</a>
+    <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-4 items-center">
+      <h1 class="font-medium text-5xl sm:text-6xl font-title">
+        <span :class="darkMode ? 'text-red-400' : 'text-red-500'">reddit</span><span :class="[darkMode ? 'text-neutral-100' : 'text-neutral-900']">le</span>
+      </h1>
+      <div class="relative block max-w-screen px-4 sm:px-0 w-[35rem]" :class="[darkMode ? 'text-neutral-200' : 'text-neutral-900']">
+        <div class="absolute inset-y-0 left-0 pl-8 sm:pl-4 flex items-center">
+          <SearchIcon class="h-5 w-5 text-neutral-400" aria-hidden="true" />
+        </div>
+        <input autofocus v-model="query" type="text" name="query" @keyup.enter="submit"
+          placeholder="Try prefixing with r/<subreddit> for a specific subreddit"
+          class="block w-full border rounded-full py-3 pl-12 hover:shadow-catche focus:shadow-around focus:ring-0 focus:border-white focus-visible:outline-none bg-transparent" :class="[darkMode ? 'border-neutral-700 placeholder:text-neutral-500' : 'border-neutral-300 placeholder:text-neutral-400']" />
+      </div>
+      <div class="flex flex-col gap-2">
+        <button class=" px-4 py-2 rounded hover:shadow-around border"
+          :class="[darkMode ? 'bg-neutral-800 text-neutral-300 border-neutral-700' : 'bg-neutral-100 text-neutral-800 border-neutral-300']"
+          @click="submit">Search</button>
+        <a class="w-max text-sm" :class="darkMode ? 'text-red-300' : 'text-red-500'" href="https://www.producthunt.com/posts/redditle" target="_blank">(Psst we're on ProductHunt!)</a>
+      </div>
     </div>
   </div>
 </template>
@@ -21,14 +33,33 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { SearchIcon } from '@heroicons/vue/outline'
+import { SunIcon, MoonIcon } from '@heroicons/vue/solid'
+import { Switch } from '@headlessui/vue'
 
 export default defineComponent({
+  mounted () {
+    const darkMode = localStorage.getItem('darkMode')
+    if (darkMode === 'true') this.darkMode = true
+  },
   components: {
     SearchIcon,
+    SunIcon,
+    MoonIcon,
+    Switch,
   },
   data () {
     return {
       query: '',
+      darkMode: false,
+    }
+  },
+  watch: {
+    darkMode (darkMode) {
+      if (darkMode) {
+        localStorage.setItem('darkMode', 'true')
+      } else {
+        localStorage.setItem('darkMode', 'false')
+      }
     }
   },
   methods: {
--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,26 +1,39 @@
 <template>
-  <!-- Top -->
-  <div class="flex flex-col sm:flex-row items-center py-4 sm:py-6 border border-white gap-2 sm:gap-0 fixed bg-white top-0 w-full text-gray-800"
-    :class="isAtTop ? 'border-b-2 border-gray-100' : 'shadow-md'">
-    <a href="/" class="font-medium text-2xl font-title mx-2 sm:mx-8">
-      <span class="text-red-500">reddit</span>le
-    </a>
-    <div class="relative block max-w-screen px-4 sm:px-0 w-[35rem]">
-      <input autofocus v-model="query" type="text" name="query" @keyup.enter="submit"
-        class="block w-full border border-gray-300 rounded-full py-2 sm:py-3 pl-8 hover:shadow-around focus:shadow-around focus:ring-0 focus:border-0 focus-visible:outline-none" />
-      <div class="absolute inset-y-0 right-0 pr-8 sm:pr-4 items-center flex">
-        <XIcon class="h-5 w-5 text-gray-400 border-gray-200 cursor-pointer" @click="clear" aria-hidden="true" :class="loading ? 'animate-spin' : ''" />
-        <div class="h-3/5 w-0.5 bg-gray-100 mx-2"></div>
-        <SearchIcon class="h-5 w-5 text-light-blue cursor-pointer" @click="submit" aria-hidden="true" />
+  <div class="min-h-screen" :class="[darkMode ? 'bg-neutral-800 text-neutral-300' : 'bg-white text-neutral-900']">
+    <div class="fixed z-50 w-full">
+      <div class="flex gap-1 items-center justify-end p-2">
+        <SunIcon class="h-5" :class="[darkMode ? 'text-neutral-600' : 'text-neutral-300']" />
+        <Switch v-model="darkMode" :class="[darkMode ? 'bg-neutral-600' : 'bg-neutral-200', 'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-0']">
+          <span class="sr-only">Use setting</span>
+          <span aria-hidden="true" :class="[darkMode ? 'translate-x-5' : 'translate-x-0', 'pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200']" />
+        </Switch>
+        <MoonIcon class="h-5" :class="[darkMode ? 'text-neutral-600' : 'text-neutral-300']" />
       </div>
     </div>
-    <div class="px-4 flex gap-2">
-      <input class="cursor-pointer" type="checkbox" id="oldreddit" v-model="useOldReddit" />
-      <label class="cursor-pointer" for="oldreddit">Use old.reddit.com</label>
+  <!-- Top -->
+  <div class="flex flex-col sm:flex-row items-center justify-between pt-10 pb-4 sm:py-6 gap-2 sm:gap-0 fixed top-0 w-full"
+    :class="[darkMode ? 'bg-neutral-800 text-neutral-300' : 'bg-white text-neutral-900', darkMode ? (isAtTop ? 'border-b-2 border-neutral-700' : 'shadow-md') : (isAtTop ? 'border-b-2 border-neutral-100' : 'shadow-md')]">
+    <div class="flex flex-col sm:flex-row items-center">
+      <a href="/" class="font-medium text-2xl font-title mx-2 sm:mx-8">
+        <span :class="darkMode ? 'text-red-400' : 'text-red-500'">reddit</span>le
+      </a>
+      <div class="relative block max-w-screen px-4 sm:px-0 w-[35rem]">
+        <input autofocus v-model="query" type="text" name="query" @keyup.enter="submit"
+          class="block w-full border rounded-full py-2 sm:py-3 pl-8 hover:shadow-around focus:shadow-around focus:ring-0 focus-visible:outline-none bg-transparent" :class="darkMode ? ' focus:border-neutral-600 border-neutral-600' : 'focus:border-0 border-neutral-300'" />
+        <div class="absolute inset-y-0 right-0 pr-8 sm:pr-4 items-center flex">
+          <XIcon class="h-5 w-5 text-neutral-400 border-neutral-200 cursor-pointer" @click="clear" aria-hidden="true" :class="loading ? 'animate-spin' : ''" />
+          <div class="h-3/5 w-0.5 mx-2" :class="darkMode ? 'bg-neutral-600' : 'bg-neutral-200'"></div>
+          <SearchIcon class="h-5 w-5 cursor-pointer" @click="submit" aria-hidden="true" :class="darkMode ? 'text-dark-blue' : 'text-light-blue'" />
+        </div>
+      </div>
+      <div class="px-4 py-1 flex gap-2 items-center">
+        <input class="cursor-pointer rounded bg-transparent focus:ring-0" :class="darkMode ? 'checked:bg-red-400 focus:checked:bg-red-400 hover:checked:bg-red-400' : 'checked:bg-red-500 focus:checked:bg-red-500 hover:checked:bg-red-500'" type="checkbox" id="oldreddit" v-model="useOldReddit" />
+        <label class="cursor-pointer" for="oldreddit">Use old.reddit.com</label>
+      </div>
     </div>
   </div>
   <!-- Results -->
-  <div class="max-w-screen sm:max-w-2xl px-4 sm:px-0 sm:ml-48 mt-44 sm:mt-32 text-gray-800">
+  <div class="max-w-screen sm:max-w-2xl px-4 sm:px-0 sm:ml-48 pt-44 sm:pt-32" :class="darkMode ? 'text-neutral-300' : 'text-neutral-800'">
     <div v-if="loading" class="my-4 text-sm">
       Loading...
     </div>
@@ -28,7 +41,7 @@
       <div class="my-4 text-sm">{{stats}}</div>
       <div class="flex flex-col gap-8">
         <div v-for="(result, i) in results" :key="i">
-          <a :href="useOldReddit ? result.href.replace('www.reddit.com', 'old.reddit.com') : result.href" class="text-link-blue text-xl">{{ result.title }}</a>
+          <a :href="useOldReddit ? result.href.replace('www.reddit.com', 'old.reddit.com') : result.href" class="text-xl" :class="darkMode ? 'text-dark-blue' : 'text-link-blue'">{{ result.title }}</a>
           <p>{{ result.desc }}</p>
         </div>
       </div>
@@ -41,52 +54,55 @@
     </div>
   </div>
   <!-- Footer -->
-  <div class="max-w-screen sm:max-w-2xl px-4 sm:px-0 sm:ml-48 mt-20 mb-36 text-gray-800">
+  <div class="max-w-screen sm:max-w-2xl px-4 sm:px-0 sm:ml-48 mt-20 pb-36 text-neutral-800">
     <h1 class="font-medium text-3xl font-title mx-8 flex w-max mx-auto">
       <!-- Prev -->
       <div v-if="hasPrev" class="inline px-0 mx-0 flex flex-col w-max items-center mx-2 cursor-pointer" @click="paginate(currentPage-1)">
         <div class="flex items-center">
           &nbsp;
-          <ChevronLeftIcon class="h-5 w-5 cursor-pointer" aria-hidden="true" />
+          <ChevronLeftIcon class="h-5 w-5 cursor-pointer" aria-hidden="true" :class="darkMode ? 'text-gray-300' : ''" />
           &nbsp;
         </div>
-        <div class="text-sm text-light-blue font-sans">Prev</div>
+        <div class="text-sm font-sans" :class="darkMode ? 'text-dark-blue' : 'text-light-blue'">Prev</div>
       </div>
-      <span class="text-red-500">re</span>
+      <span :class="darkMode ? 'text-red-400' : 'text-red-500'">re</span>
       <div v-for="p in Math.min(currentPage, 9)" :key="p" class="inline px-0 mx-0 flex flex-col w-max items-center"
-        :class="currentPage === p + paginateStart ? '' : 'cursor-pointer text-red-500'" @click="paginate(p + paginateStart)">
+        :class="currentPage === p + paginateStart ? (darkMode ? 'text-neutral-300' : '') : (darkMode ? 'cursor-pointer text-red-400' : 'cursor-pointer text-red-500')" @click="paginate(p + paginateStart)">
         d
-        <div class="text-sm font-sans" :class="currentPage === p + paginateStart ? '' : 'text-light-blue'">
+        <div class="text-sm font-sans" :class="currentPage === p + paginateStart ? '' : (darkMode ? 'text-dark-blue' : 'text-light-blue')">
           {{ p + paginateStart }}
         </div>
       </div>
-      <div v-if="hasNext" class="inline px-0 mx-0 flex flex-col w-max items-center cursor-pointer text-red-500"
+      <div v-if="hasNext" class="inline px-0 mx-0 flex flex-col w-max items-center cursor-pointer" :class="darkMode ? 'text-red-400' : 'text-red-500'"
         @click="paginate(currentPage+1)">
         d
-        <div class="text-sm font-sans text-light-blue">
+        <div class="text-sm font-sans" :class="darkMode ? 'text-dark-blue' : 'text-light-blue'">
           ...
         </div>
       </div>
-      <div v-else class="inline px-0 mx-0 flex flex-col w-max items-center text-red-500">
+      <div v-else class="inline px-0 mx-0 flex flex-col w-max items-center" :class="darkMode ? 'text-red-400' : 'text-red-500'">
         d
       </div>
-      <span class="text-red-500">it</span>le
+      <span :class="darkMode ? 'text-red-400' : 'text-red-500'">it</span><span :class="[darkMode ? 'text-neutral-300' : 'text-neutral-900']">le</span>
       <!-- Next -->
       <div v-if="hasNext" class="inline px-0 mx-0 flex flex-col w-max items-center mx-2 cursor-pointer" @click="paginate(currentPage+1)">
         <div class="flex items-center">
           &nbsp;
-          <ChevronRightIcon class="h-5 w-5 cursor-pointer" aria-hidden="true" />
+          <ChevronRightIcon class="h-5 w-5 cursor-pointer" aria-hidden="true" :class="darkMode ? 'text-gray-300' : ''" />
           &nbsp;
         </div>
-        <div class="text-sm text-light-blue font-sans">Next</div>
+        <div class="text-sm font-sans" :class="darkMode ? 'text-dark-blue' : 'text-light-blue'">Next</div>
       </div>
     </h1>
+  </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { XIcon, SearchIcon, ChevronLeftIcon, ChevronRightIcon } from '@heroicons/vue/outline'
+import { SunIcon, MoonIcon } from '@heroicons/vue/solid'
+import { Switch } from '@headlessui/vue'
 
 interface Result {
   title: string;
@@ -98,6 +114,8 @@ export default defineComponent({
   mounted () {
     const useOldReddit = localStorage.getItem('useOldReddit')
     if (useOldReddit === 'true') this.useOldReddit = true
+    const darkMode = localStorage.getItem('darkMode')
+    if (darkMode === 'true') this.darkMode = true
     window.addEventListener("scroll", () => {
       this.isAtTop = window.scrollY === 0
     }, false)
@@ -111,6 +129,9 @@ export default defineComponent({
     SearchIcon,
     ChevronLeftIcon,
     ChevronRightIcon,
+    SunIcon,
+    MoonIcon,
+    Switch,
   },
   data () {
     return {
@@ -126,6 +147,7 @@ export default defineComponent({
       hasPrev: false,
       hasNext: false,
       useOldReddit: false,
+      darkMode: false,
     }
   },
   computed: {
@@ -140,11 +162,18 @@ export default defineComponent({
     },
   },
   watch: {
-    useOldReddit (newValue) {
-      if (newValue) {
+    useOldReddit (useOldReddit) {
+      if (useOldReddit) {
         localStorage.setItem('useOldReddit', 'true')
       } else {
         localStorage.setItem('useOldReddit', 'false')
+      }
+    },
+    darkMode (darkMode) {
+      if (darkMode) {
+        localStorage.setItem('darkMode', 'true')
+      } else {
+        localStorage.setItem('darkMode', 'false')
       }
     }
   },
